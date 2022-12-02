490a5a201e85777ab4b39ae265ffbf721b854e59
Zongzi <cs.zongzi@gmail.com>
2022-12-02 22:11:03 +0800

    fix: NextIntlProvider no longer exists

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,6 @@
-import { NextIntlProvider } from 'next-intl';
 import type { AppProps } from 'next/app';
 import './_app.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <NextIntlProvider messages={pageProps.messages}>
-      <Component {...pageProps} />
-    </NextIntlProvider>
-  );
+  return <Component {...pageProps} />;
 }
